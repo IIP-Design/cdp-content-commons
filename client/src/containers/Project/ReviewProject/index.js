5680b7e489f6e10cfb30ea032690428fc2e886ca
Shawn Mathew <shawn.mathew@agencychief.com>
2018-09-17 17:27:45 -0400

    add project files layout

--- client/src/containers/Project/ReviewProject/index.js
+++ client/src/containers/Project/ReviewProject/index.js
@@ -11,16 +11,18 @@ import * as actions from './actions';
 import makeSelectReviewProject from './selectors';
 import Page from 'components/Page';
 import Breadcrumbs from 'components/Breadcrumbs';
-import { Grid, Button } from 'semantic-ui-react';
+import { Grid, Button, Checkbox } from 'semantic-ui-react';
+import sampleThumbSM from '../../../assets/images/Projects/madeinamerica_small.jpg';
+import sampleThumbMD from '../../../assets/images/Projects/madeinamerica_med.jpg';
 import './ReviewProject.css';
 
 const ReviewProject = ( props ) => {
   return (
     <Page title="Review Project" description="Review content project">
       <Breadcrumbs />
-
       <div className="review-project">
-        <section className="review-project_header section">
+
+        <section className="section section--project_header">
           <Grid stackable divided="vertically">
             <Grid.Row>
               <Grid.Column mobile={ 16 } computer={ 8 }>
@@ -36,7 +38,7 @@ const ReviewProject = ( props ) => {
           </Grid>
         </section>
 
-        <section className="review-project_project-data section">
+        <section className="section section--project_data">
           <Grid stackable divided>
             <Grid.Row>
               <Grid.Column className="project-data_info" mobile={ 16 } computer={ 10 }>
@@ -69,19 +71,66 @@ const ReviewProject = ( props ) => {
                   <p className="file"><span className="label">English:</span> madeinamerica_english.srt</p>
                   <p className="file"><span className="label">French:</span> madeinamerica_french.srt</p>
                 </section>
+                <section className="thumbnails section">
+                  <p className="label">Thumbnail files</p>
+                  <Grid columns='three'>
+                    <Grid.Column>
+                      <img className="thumbnails_img" src={ sampleThumbSM } alt="Project Thumbnail Small" />
+                      <p>Arabic</p>
+                    </Grid.Column>
+                    <Grid.Column>
+                      <img className="thumbnails_img" src={ sampleThumbSM } alt="Project Thumbnail Small" />
+                      <p>Chinese</p>
+                    </Grid.Column>
+                    <Grid.Column>
+                      <img className="thumbnails_img" src={ sampleThumbSM } alt="Project Thumbnail Small" />
+                      <p>English</p>
+                    </Grid.Column>
+                    <Grid.Column>
+                      <img className="thumbnails_img" src={ sampleThumbSM } alt="Project Thumbnail Small" />
+                      <p>French</p>
+                    </Grid.Column>
+                  </Grid>
+                  <Checkbox label="Disable right-click to protect your images" />
+                </section>
               </Grid.Column>
             </Grid.Row>
           </Grid>
         </section>
 
-        <section className="review-project_project-files section">
-          <Grid stackable divided="vertically">
-            <Grid.Row>
-              <Grid.Column>
-                <h3>VIDEOS IN PROJECT</h3>                
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
+        <section className="section section--project_files project_files">
+          <h3 className="project_files_headline">VIDEOS IN PROJECT</h3>
+          <div className="project_file">
+            <Grid>
+              <Grid.Row className="project_file_header">
+                <Grid.Column floated="left" mobile={ 8 }>
+                  <h3 className="title">Made in America</h3>
+                </Grid.Column>
+                <Grid.Column floated="right" mobile={ 8 } className="project_file_edit">
+                  <Button>Edit</Button>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row className="project_file_contents">
+                <Grid.Column mobile={ 16 } tablet={ 8 } computer={ 8 } className="file_meta">
+                  <img src={ sampleThumbMD } alt="Project File" />
+                  <p><span className="label">File Name:</span> madeinamerica_english.mp4</p>
+                  <p><span className="label">Filesize:</span> 631.9MB</p>
+                  <p><span className="label">Dimensions:</span> 1920 x 1080</p>
+                  <p><span className="label">Uploaded:</span> April 13, 2018 at 3:45 PM</p>
+                  <p><span className="label">Duration:</span> 9:16</p>
+                </Grid.Column>
+                <Grid.Column mobile={ 16 } tablet={ 8 } computer={ 8 } className="file_info">
+                  <p><span className="label">Language:</span> English</p>
+                  <p><span className="label">Subtitles & Captions:</span> Clean – No Captions</p>
+                  <p><span className="label">Video Type:</span> Full Video</p>
+                  <p><span className="label">Quality:</span> For web</p>
+                  <p className="public_description"><span className="label">Public Description:</span> The value and meaning of the words “Made in America” come from a rich history of innovation and perseverance. Stay for a brief history of manufacturing in the USA and hear some of the positive impacts it has brought to the world and how its benefits are felt today.</p>
+                  <p><span className="label">YouTube URL:</span> youtube.com/videourl</p>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+
+          </div>
         </section>
 
       </div>
