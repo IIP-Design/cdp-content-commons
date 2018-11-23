099f4068a2bdf8521e08abfd8233e726e9a4b080
Edwin Mah <me@edwinmah.com>
2018-11-23 08:42:56 -0500

    Add initial implementation of VideoItem progress bars

--- client/src/components/Project/Types/VideoItem/index.js
+++ client/src/components/Project/Types/VideoItem/index.js
@@ -5,7 +5,9 @@
  */
 
 import React from 'react';
-import { object, string } from 'prop-types';
+import { array, object, string } from 'prop-types';
+import { Icon, Progress } from 'semantic-ui-react';
+
 import './VideoItem.css';
 
 const VideoItem = ( props ) => {
@@ -15,8 +17,18 @@ const VideoItem = ( props ) => {
     thumbnail,
     alt,
     fileName,
+    source,
     ...rest
   } = props;
+
+  const { filesize } = source[0].size;
+
+  /**
+   * @todo will need to replace `true` condition with
+   * state value of whether upload is complete
+   * (i.e., when total filesize === total uploaded)
+   */
+  const isUploading = true;
 
   /**
    * Duplicate props to avoid unknown prop warning
@@ -29,15 +41,38 @@ const VideoItem = ( props ) => {
   const itemStyle = {
     flexBasis: '15em',
     marginRight: '1em',
-    cursor: 'pointer'
+    cursor: isUploading ? 'not-allowed' : 'pointer'
   };
 
   return (
     <li className="item video" style={ itemStyle } { ...itemProps }>
-      <div className="thumbnail">
+      <div className={ `thumbnail${isUploading ? ' uploading' : ''}` }>
         <img src={ thumbnail } alt={ alt } />
         <p className="file-name">{ fileName }</p>
+        { isUploading &&
+          <div className="loading-animation">
+            <Icon
+              loading
+              name="spinner"
+              size="big"
+            />
+          </div> }
       </div>
+      { isUploading &&
+        <Progress
+          /**
+           * @todo determine value via passed-in state
+           */
+          value={ filesize * 0.72 }
+          total={ filesize }
+          color="blue"
+          size="small"
+          active
+          progress
+          precision={ 0 }
+        >
+          <p>Upload in progress</p>
+        </Progress> }
       <h3 className={ `item-heading ${language.text_direction}` }>{ title }</h3>
       <p className="item-lang">{ language.display_name }</p>
     </li>
@@ -50,6 +85,7 @@ VideoItem.propTypes = {
   thumbnail: string,
   alt: string,
   fileName: string,
+  source: array,
   rest: object
 };
 
