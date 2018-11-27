f4737293d7167e4245fcebbf98907a97515f3fe9
mrewers <mrewers@users.noreply.github.com>
2018-11-27 17:33:40 -0500

    Add thumbnail image hover overlay

--- client/src/containers/Project/EditProject/VideoEditVideo/index.js
+++ client/src/containers/Project/EditProject/VideoEditVideo/index.js
@@ -29,8 +29,11 @@ class VideoEditVideo extends React.PureComponent {
 
     return (
       <div>
-        <figure className="modal_thumbnail">
-          <img src={ data.thumbnail } alt={ data.alt } />
+        <figure className="modal_thumbnail overlay-image">
+          <img className="image" src={ data.thumbnail } alt={ data.alt } />
+          <div className="hover">
+            <div className="text">Change Thumbnail</div>
+          </div>
         </figure>
         <section className="modal_section modal_section--metaContent">
           <div className="modal_meta_wrapper">
@@ -58,7 +61,7 @@ class VideoEditVideo extends React.PureComponent {
           <Tab menu={ { secondary: true, pointing: true } } panes={ panes } />
         </section>
         <section>
-          <h3>Videos in this Project</h3>
+          <h3 className="video-carousel-header">Videos in this Project</h3>
         </section>
       </div>
     );
