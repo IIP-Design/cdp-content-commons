63fc3460865c0160c27b5b73f46fbc57d0a9bb9c
Edwin Mah <me@edwinmah.com>
2018-12-14 08:52:22 -0500

    Refactor VideoEditProject selectors

--- client/src/containers/Project/EditProject/VideoEditProject/selectors.js
+++ client/src/containers/Project/EditProject/VideoEditProject/selectors.js
@@ -6,24 +6,18 @@ import { createSelector } from 'reselect';
 const selectCurrentVideoEditProject = ( state, props ) =>
   state.projects[props.match.params.videoID];
 
-const selectUploadedVideoProjectItemsCount = ( state ) => {
-  const videos = state.videoProjectItems;
-  const videoIds = Object.keys( videos );
-  const uploadedVideos = videoIds.filter( id => !videos[id].loading && videos[id].uploadStatus.success );
-  return uploadedVideos.length;
-};
+const selectUploadedVideoProjectItemsCount = state => (
+  Object.values( state.videoProjectItems )
+    .filter( video => !video.loading && video.uploadStatus.success ).length
+);
 
-const selectUploadedSupportFilesCount = ( state ) => {
-  const supportItems = state.projectSupportItems;
-  const fileTypes = Object.values( supportItems );
-
-  const countsPerFiletype = fileTypes.map( ( items ) => {
-    const ids = Object.keys( items );
-    return ids.filter( id => !items[id].loading && items[id].uploadStatus.success ).length;
-  } );
-
-  return countsPerFiletype.reduce( ( acc, curr ) => acc + curr, 0 );
-};
+const selectUploadedSupportFilesCount = state => (
+  Object.values( state.projectSupportItems )
+    .map( items => Object.values( items )
+      .filter( item => !item.loading && item.uploadStatus.success )
+      .length )
+    .reduce( ( acc, curr ) => acc + curr, 0 )
+);
 
 /**
  * Selector factories: returns selector instances
