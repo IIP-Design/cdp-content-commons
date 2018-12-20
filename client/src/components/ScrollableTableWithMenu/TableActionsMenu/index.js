449163e95575c88671b0e5b46497760eb3662219
Shawn Mathew <shawn.mathew@agencychief.com>
2018-12-20 13:36:22 -0500

    add icons and style actions menu

--- client/src/components/ScrollableTableWithMenu/TableActionsMenu/index.js
+++ client/src/components/ScrollableTableWithMenu/TableActionsMenu/index.js
@@ -6,7 +6,11 @@
 
 import React from 'react';
 // import PropTypes from 'prop-types';
-import { Icon } from 'semantic-ui-react';
+import editIcon from 'assets/images/Dashboard/edit.svg'
+import createIcon from 'assets/images/Dashboard/create.svg'
+import deleteIcon from 'assets/images/Dashboard/delete.svg'
+import archiveIcon from 'assets/images/Dashboard/archive.svg'
+import unpublishIcon from 'assets/images/Dashboard/unpublish.svg'
 import './TableActionsMenu.css';
 
 /* eslint-disable react/prefer-stateless-function */
@@ -15,12 +19,12 @@ class TableActionsMenu extends React.Component {
     const { displayActionsMenu } = this.props;
     return (
       <div className={ displayActionsMenu ? 'actionsMenu active' : 'actionsMenu' }>
-        <Icon name='pencil' size='big' />
-        <Icon name='trash' size='big' />
-        <Icon name='eye' size='big' />
-        <Icon name='plus square outline' size='big' />
-        <Icon name='briefcase' size='big' />
-        | <span>Unpublish</span>
+        <img src={ editIcon } alt='Edit Selection(s)' title='Edit Selection(s)'/>
+        <img src={ deleteIcon } alt='Delete Selection(s)' title='Delete Selection(s)'/>
+        <img src={ unpublishIcon } alt='Unpublish Selection(s)' title='Unpublish Selection(s)'/>
+        <img src={ createIcon } alt='Create Selection(s)' title='Create Selection(s)'/>
+        <img src={ archiveIcon } alt='Archive Selection(s)' title='Archive Selection(s)'/>        
+        <div className="unpublish">|<span className="unpublish--text">Unpublish</span></div>
       </div>
     );
   }
