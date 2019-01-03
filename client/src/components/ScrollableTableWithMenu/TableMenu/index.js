094fcff2149216784fa44f166087bb829f8c0ebd
Shawn Mathew <shawn.mathew@agencychief.com>
2019-01-03 14:23:03 -0500

    scroll table with left/right onClick fn's

--- client/src/components/ScrollableTableWithMenu/TableMenu/index.js
+++ client/src/components/ScrollableTableWithMenu/TableMenu/index.js
@@ -38,6 +38,16 @@ class TableMenu extends React.Component {
     this.setState( prevState => ({ displayTableMenu: false }) );
   }
 
+  handleTableScroll = e => {
+    const items_table = document.querySelector('.items_table');
+    const tableArrowDirection = e.target.dataset.tablearrow;    
+    { 
+      tableArrowDirection === 'right'
+      ? items_table.scrollLeft += 200
+      : items_table.scrollLeft -= 200
+    }
+  }
+
   render() {
     const { displayTableMenu } = this.state;
     const { columnMenu, tableMenuOnChange } = this.props;
@@ -46,8 +56,12 @@ class TableMenu extends React.Component {
       <Grid.Column floated='right' width={ 8 } className="items_menu_wrapper">
         <div className={ displayTableMenu ? 'items_menu active' : 'items_menu' }>
           <span data-tablemenu>See More <Icon data-tablemenu name="angle down"/></span>          
-          <span><Icon name="angle left" /></span>
-          <span><Icon name="angle right" /></span>
+          <span data-tablearrow='left' onClick={ this.handleTableScroll }>
+            <Icon name="angle left" data-tablearrow='left' />
+          </span>
+          <span data-tablearrow='right' onClick={ this.handleTableScroll }>
+            <Icon name="angle right" data-tablearrow='right' />
+          </span>
           <div className={ displayTableMenu ? 'items_menu_list display' : 'items_menu_list' }>
             { columnMenu.map( item => (
               <Checkbox 
